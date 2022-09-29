a6dc2bb49cf4a8dd4664e97201bd10c477da04c6
JI-NAMEUN <skadms1720@gmail.com>
2022-09-29 23:07:45 +0900

    매번 다른실행

--- src/main/java/com/ll/exam/app_2022_09_22/App20220922Application.java
+++ src/main/java/com/ll/exam/app_2022_09_22/App20220922Application.java
@@ -1,13 +1,13 @@
 package main.java.com.ll.exam.app_2022_09_22;
-
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableBatchProcessing
 @SpringBootApplication
 public class App20220922Application {
-
     public static void main(String[] args) {
         SpringApplication.run(App20220922Application.class, args);
     }
-
 }
--- src/main/java/com/ll/exam/app_2022_09_22/EnableBatchProcessing.java
+++ src/main/java/com/ll/exam/app_2022_09_22/EnableBatchProcessing.java
@@ -1,2 +1,4 @@
-package main.java.com.ll.exam.app_2022_09_22;public @interface EnableBatchProcessing {
+package main.java.com.ll.exam.app_2022_09_22;
+
+public @interface EnableBatchProcessing {
 }
