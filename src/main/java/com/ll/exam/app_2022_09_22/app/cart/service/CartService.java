c008947b6898c1f3157fa9d0e475466e56f3c3d2
JI-NAMEUN <skadms1720@gmail.com>
2022-10-04 16:20:38 +0900

    26강,장바구니에 한 회원이 같은 상품옵션을 2번 이상 넣으면 그 때 부터는 기존 아이템의 수량만 변화

--- src/main/java/com/ll/exam/app_2022_09_22/app/cart/service/CartService.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/cart/service/CartService.java
@@ -6,19 +6,29 @@ import com.ll.exam.app_2022_09_22.app.member.entity.Member;
 import com.ll.exam.app_2022_09_22.app.product.entity.ProductOption;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class CartService {
     private final CartItemRepository cartItemRepository;
 
-    public void addItem(Member member, ProductOption productOption, int quantity) {
-        CartItem cartItem = CartItem.builder()
+    public CartItem addItem(Member member, ProductOption productOption, int quantity) {
+        CartItem oldCartItem = cartItemRepository.findByMemberIdAndProductOptionId(member.getId(), productOption.getId()).orElse(null);
+
+        if ( oldCartItem != null ) {
+            oldCartItem.setQuantity(oldCartItem.getQuantity() + quantity);
+            cartItemRepository.save(oldCartItem);
+
+            return oldCartItem;
+        }
+                CartItem cartItem = CartItem.builder()
                 .member(member)
                 .productOption(productOption)
                 .quantity(quantity)
                 .build();
 
         cartItemRepository.save(cartItem);
+
+        return cartItem;
     }
 }
